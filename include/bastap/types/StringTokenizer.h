a813d9ab84902207e5a572d9b1fe819f51dc5023
Juraj Ďurech <durech.juraj@gmail.com>
2018-03-23 21:05:33 +0100

    Unfinished StringTokenizer

---
+++ include/bastap/types/StringTokenizer.h
@@ -0,0 +1,135 @@
+//
+// Copyright 2018 Juraj Durech <durech.juraj@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <assert.h>
+
+namespace bastap
+{
+	class StringTokenizer
+	{
+	public:
+		
+		// MARK: - Supporting types
+		
+		typedef std::string::const_iterator		iterator;
+		typedef std::string::difference_type	difference;
+		
+		struct Range
+		{
+			iterator begin;
+			iterator end;
+			
+			std::string content() {
+				return std::string(begin, end);
+			}
+		};
+		
+		struct State
+		{
+			iterator	pos;
+			Range		line;
+			Range		capture;
+			bool		updateLineEnd;
+			size_t		lineNumber;
+		};
+		
+		struct PositionInfo
+		{
+			size_t lineNumber;
+			size_t offsetAtLine;
+		};
+		
+		// MARK: - Public methods
+		
+		StringTokenizer(const iterator begin, const iterator end);
+		StringTokenizer(const Range range);
+		
+		/// Resets position to the beginning of string
+		void reset();
+		
+		/// Changes behavior of isEnd() method, which will report end at the end of line.
+		/// In this mode, you need to call nextLine() to go to the next line
+		void setStopAtLineEnd(bool stop);
+		
+		/// Returns value previously set in `setStopAtLineEnd()`
+		bool isStopAtLineEnd() const;
+		
+		/// Stores a whole state of
+		State storeState() const;
+		void restoreState(const State & state);
+		
+		/// Returns true if tokenizer is at the end of line
+		bool isEnd() const;
+		
+		/// Returns char at current position and moves to the next position.
+		/// If 0 is returned, then we're at the end.
+		char getChar();
+		
+		/// Returns char at offset to current position, or 0 if offset is out of range.
+		char charAt(difference offset = 0) const;
+		
+		/// Moves currsor by given offset. Returns true if that operation is possible, or
+		/// false if offset is out of range.
+		bool movePosition(difference offset = 1);
+		
+		/// Go immediately to the next line
+		bool nextLine();
+		
+		/// Skip whitespace characters. The current position will end at first non-whitespace character or at the end.
+		bool skipWhitespace();
+		
+		/// Skips characters until function returns false or the end is reached. The function receives consecutive characters
+		/// in its paramter.
+		bool skipUntil(int (*function)(int));
+		
+		/// 
+		bool searchFor(int (*function)(int));
+
+		/// Returns range for current line.
+		Range line();
+		
+		/// Sets capture begin and end to current position.
+		void resetCapture();
+		
+		/// Sets capture end to current position and returns reference to current range.
+		Range capture();
+		
+		/// Returns current capture and then resets capture range to current position
+		Range captureAndReset();
+		
+		PositionInfo positionInfo();
+		
+		
+	private:
+
+		bool isRealEnd() const;
+		char realGetChar();
+		char realCharAt(difference offset) const;
+		
+		void updateLineEnd();
+		
+		const iterator	_str_begin;
+		const iterator	_str_end;
+		bool			_stop_at_lf;
+		
+		State _state;
+		
+	};
+}
---
+++ source/library/types/StringTokenizer.cpp
@@ -0,0 +1,225 @@
+//
+// Copyright 2018 Juraj Durech <durech.juraj@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#include <bastap/types/StringTokenizer.h>
+#include <assert.h>
+#include <ctype.h>
+
+namespace bastap
+{
+
+#define _CHECK_LE() assert(_state.updateLineEnd == false);
+
+	// MARK: - Public methods
+	StringTokenizer::StringTokenizer(const iterator begin, const iterator end) :
+		_str_begin(begin),
+		_str_end(end),
+		_stop_at_lf(false),
+		_state({
+			begin,
+			{begin, begin},
+			{begin, begin},
+			0
+		})
+	{
+	}
+	
+	StringTokenizer::StringTokenizer(const Range range) :
+		StringTokenizer(range.begin, range.end)
+	{
+	}
+	
+	void StringTokenizer::reset()
+	{
+		_state.pos 				= _str_begin;
+		_state.line.begin 		= _str_begin;
+		_state.line.end 		= _str_begin;
+		_state.updateLineEnd	= true;
+		_state.lineNumber		= 0;
+		
+		if (_stop_at_lf) {
+			updateLineEnd();
+		}
+	}
+	
+	void StringTokenizer::setStopAtLineEnd(bool stop)
+	{
+		_stop_at_lf = stop;
+	}
+	
+	bool StringTokenizer::isStopAtLineEnd() const
+	{
+		return _stop_at_lf;
+	}
+	
+	bool StringTokenizer::isEnd() const
+	{
+		if (!_stop_at_lf) {
+			return isRealEnd();
+		}
+		_CHECK_LE();
+		return _state.pos == _state.line.end;
+	}
+	
+	char StringTokenizer::getChar()
+	{
+		if (!isEnd()) {
+			return *(_state.pos++);
+		}
+		return 0;
+	}
+	
+	char StringTokenizer::charAt(difference offset) const
+	{
+		auto begin = _stop_at_lf ? _state.line.begin : _str_begin;
+		auto end   = _stop_at_lf ? _state.line.end : _str_end;
+		if (offset > 0) {
+			if (offset >= std::distance(_state.pos, end)) {
+				return 0;
+			}
+		} else {
+			if (offset < std::distance(_state.pos, begin)) {
+				return 0;
+			}
+		}
+		return *(_state.pos + offset);
+	}
+	
+	char StringTokenizer::realCharAt(difference offset) const
+	{
+		if (offset > 0) {
+			if (offset >= std::distance(_state.pos, _str_end)) {
+				return 0;
+			}
+		} else {
+			if (offset < std::distance(_state.pos, _str_begin)) {
+				return 0;
+			}
+		}
+		return *(_state.pos + offset);
+	}
+
+	//
+	
+	bool StringTokenizer::isRealEnd() const
+	{
+		return _state.pos == _str_end;
+	}
+	
+	char StringTokenizer::realGetChar()
+	{
+		if (!isRealEnd()) {
+			return *(_state.pos++);
+		}
+		return 0;
+	}
+	
+	bool StringTokenizer::nextLine()
+	{
+		if (!_stop_at_lf) {
+			updateLineEnd();
+		} else {
+			_CHECK_LE();
+		}
+
+		char c = realGetChar();
+		if (c != 0) {
+			if (c == '\n') {
+				// UNIX, do nothing...
+			} else if (c == '\r') {
+				// Handle crappy MS-DOS & Descendants
+				c = realGetChar();
+				if (c != '\n') {
+					// TODO: throw an error, this is not right combination
+					fprintf(stderr, "Invalid CR-LF sequence\n");
+					_state.pos--;
+				}
+			}
+			_state.lineNumber++;
+		}
+		
+		_state.updateLineEnd = true;
+		if (_stop_at_lf) {
+			updateLineEnd();
+			resetCapture();
+		}
+		
+		return isRealEnd();
+	}
+	
+	bool StringTokenizer::skipWhitespace()
+	{
+		return skipUntil(isspace);
+	}
+	
+	bool StringTokenizer::skipUntil(int (*function)(int))
+	{
+		assert(function != nullptr);
+		while (true) {
+			char c = getChar();
+			if (c == 0) {
+				return false;
+			}
+			if (!function(c)) {
+				return true;
+			}
+		}
+	}
+	
+	StringTokenizer::Range StringTokenizer::line()
+	{
+		updateLineEnd();
+		return _state.line;
+	}
+	
+	void StringTokenizer::resetCapture()
+	{
+		_state.capture.begin 	= _state.pos;
+		_state.capture.end 		= _state.pos;
+	}
+	
+	StringTokenizer::Range StringTokenizer::capture()
+	{
+		_state.capture.end = _state.pos;
+		return _state.capture;
+	}
+	
+	StringTokenizer::Range StringTokenizer::captureAndReset()
+	{
+		auto c = capture();
+		_state.capture.begin = _state.capture.end;
+		return c;
+	}
+	
+	// MARK: - Private methods
+	
+	void StringTokenizer::updateLineEnd()
+	{
+		if (_state.updateLineEnd) {
+			_state.line.end = _state.line.begin;
+			while (_state.line.end != _str_end) {
+				auto c = *_state.line.end;
+				if (c == '\n' || c == '\r') {
+					break;
+				}
+				++_state.line.end;
+			}
+			_state.updateLineEnd = false;
+		}
+	}
+	
+} // bastap
+
